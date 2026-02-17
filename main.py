c252aee9f22ff69ea76ab7f6f1caf6c337afa059
thebluescreen <marius.poncon@gmail.com>
2026-02-17 15:31:04 +0100

    distance read in mm + angle simualtion (b)

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from serial import Serial
 import numpy, turtle
 
 with Serial('COM5', 115200, timeout=1) as ser:
-    line = ser.readline()   # read a '\n' terminated line
-    while True:
+    data = []
+    b = 0
+    for i in range(0, 360):
         line = ser.readline().decode('utf-8').strip()
-        print(line)
+        if line != '':
+            if line == '0.00':
+                data.append([b, 'N/A'])  # Append infinity for "Too far"
+                b += 1
+            else:
+                data.append([b, float(line)])
+                b += 1
+        print(data)
