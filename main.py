e4a999b19c9c04d6197dc7597e559112ed3c36c6
thebluescreen <marius.poncon@gmail.com>
2026-02-17 14:57:23 +0100

    a

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import pyserial
-import numpy
+from serial import Serial
+import numpy, turtle
+
+with Serial('COM5', 115200, timeout=1) as ser:
+    line = ser.readline()   # read a '\n' terminated line
+    while True:
+        line = ser.readline().decode('utf-8').strip()
+        print(line)
